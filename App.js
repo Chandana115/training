f7672f0a15f0852864461302000957069e69a240
Chandana115 <151642839+Chandana115@users.noreply.github.com>
2025-02-13 10:26:03 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import React , {useState} from 'react';
-import ChildA from './components/ChildA';
-import ChildB from './components/ChildB';
-const Parent = ()=>{
-  const [count, setCount] = useState(0);
-  const increment = ()=>{
-    setCount(c=>c+1);
-  };
-  return(
-    <div class="b">
-      <ChildA/>
-      <ChildB count={count} increment={increment}/>
-    </div>
-  )
+import React,{createContext,useContext} from "react";
+//import Props from "./components/props";
+//create context to hold the data
+const DataContext=createContext()
+function App() {
+  const data = "Hello I am Chandana";
+  return (<div>
+    <DataContext.Provider value={data}>
+      <User1/>
+    </DataContext.Provider>
+  
+   </div> 
+  );
 }
-export default Parent;
+
+// User1 component
+function User1({ data }) {
+  return <User2 data={data} />;
+}
+
+// User2 component
+function User2({ data }) {
+  return <User3 data={data} />;
+}
+
+// User3 component
+function User3({ data }) {
+  return <User4 data={data} />;
+}
+
+// User4 component
+function User4({ data }) {
+  
+    const data1=useContext(DataContext);
+    return(
+      <div>{data1}</div>
+  );
+}
+
+export default App;
